a80b5beb6ddc7db5bdb7ad2efd7a9163222bb10b
Giulio Mesini <200260paolo@gmail.com>
2024-10-17 09:42:52 +0200

    scrivi davide scrivi

--- ParticleType.hpp
+++ ParticleType.hpp
@@ -3,7 +3,14 @@
 
 class ParticleType
 {
+public:
+    ParticleType(const char &fName, double fMass, int fCharge); // ctor
     
+
+private:
+    char *fName_;
+    double fMass_;
+    int fCharge_;
 };
 
 #endif
