12b9a7f84db47bc5250f7a71d12364c802cea064
marcomargotti <marco.margotti3@studio.unibo.it>
2024-10-17 10:20:24 +0200

    abc

--- Particle.hpp
+++ Particle.hpp
@@ -3,9 +3,21 @@
 
 #include "ParticleType.hpp"
 
-class Particle : public ParticleType
-{
-    
+// class Particle : public ParticleType {
+class Particle {
+public:
+Particle(int fIndex, double impulso_x, double impulso_y, double impulso_z);
+
+static void AddParticleType(ParticleType* particleType);
+
+private:
+int fIndex_;
+double impulso_x_;
+double impulso_y_;
+double impulso_z_;
+
+static ParticleType* particleTypes_[6];
+static int FindParticle(const char*name);
 };
 
 #endif
