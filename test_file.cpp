db82c175c4a9fcac555a2ea546318a47f79764ff
Giulio Mesini <200260paolo@gmail.com>
2024-10-19 22:53:27 +0200

    cmake

--- test_file.cpp
+++ test_file.cpp
@@ -24,6 +24,10 @@ TEST_CASE("Test of Getters functions")
   CHECK(particle[1]->GetPy() == doctest::Approx(0.5));
   CHECK(particle[1]->GetPz() == doctest::Approx(3.7));
   CHECK(particle[1]->GetMass() == doctest::Approx(1.34));
+
+  for (int i = 0; i < 2; ++i) {
+    delete particle[i];
+  }
 }
 
 TEST_CASE("Test of Energy and Invariant functions")
@@ -31,13 +35,17 @@ TEST_CASE("Test of Energy and Invariant functions")
   Particle* particle[3];
   particle[0] = new Particle("a", 0., 1., 2.);
   particle[1] = new Particle("i", 1.1, 0.5, 3.7);
-  particle[2]= new Particle("i", 1., 0., 2.);
+  particle[2] = new Particle("i", 1., 0., 2.);
 
   CHECK(particle[0]->fEnergy() == doctest::Approx(18.4758));
   CHECK(particle[0]->InvMass(*particle[2]) == doctest::Approx(20.6513));
 
   CHECK(particle[1]->fEnergy() == doctest::Approx(4.1165));
   CHECK(particle[1]->InvMass(*particle[2]) == doctest::Approx(2.8378));
+
+  for (int i = 0; i < 3; ++i) {
+    delete particle[i];
+  }
 }
 
 TEST_CASE("Test of Setters functions")
@@ -59,4 +67,8 @@ TEST_CASE("Test of Setters functions")
   CHECK(particle[1]->GetPx() == doctest::Approx(0.));
   CHECK(particle[1]->GetPy() == doctest::Approx(1.5));
   CHECK(particle[1]->GetPz() == doctest::Approx(2.));
+
+  for (int i = 0; i < 2; ++i) {
+    delete particle[i];
+  }
 }
