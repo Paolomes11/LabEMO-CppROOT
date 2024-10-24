441d446db458c06e6a871177b858af2181219c5d
Giulio Mesini <200260paolo@gmail.com>
2024-10-24 12:25:02 +0200

    Part2

--- src/random_generation.C
+++ src/random_generation.C
@@ -15,12 +15,12 @@ void random_generation()
   int Nmax  = 120;
   int nGen  = 1E5;
 
-  AddParticleType("pioni_positivi", 0.13957, 1.);
-  AddParticleType("pioni_negativi", 0.13957, -1.);
-  AddParticleType("kaoni_positivi", 0.49367, 1.);
-  AddParticleType("kaoni_negativi", 0.49367, -1.);
-  AddParticleType("protoni_positivi", 0.93827, 1.);
-  AddParticleType("protoni_negativi", 0.93827, -1.);
+  AddParticleType("Pi+", 0.13957, 1.);
+  AddParticleType("Pi-", 0.13957, -1.);
+  AddParticleType("K+", 0.49367, 1.);
+  AddParticleType("K-", 0.49367, -1.);
+  AddParticleType("P+", 0.93827, 1.);
+  AddParticleType("P-", 0.93827, -1.);
   AddParticleType("K*", 0.89166, 0., 0.050);
 
   gRandom→SetSeed();
@@ -35,25 +35,7 @@ void random_generation()
   Particle* EventParticle[N];
   for (int i = 1; i < nGen; i++) {
     for (int j = 0; j < 100; j++) {
-      // Part 3 Set name with probability
-      double chPart = TRandom::Uniform(0, 1);
-      // double chPart = TRandom::Rndm();
-      if (chPart < 0.4) {
-        EventParticle[j] = new Particle("Pi+");
-      } else if (chPart >= 0.4 && chPart < 0.8) {
-        EventParticle[j] = new Particle("Pi-");
-      } else if (chPart >= 0.8 && chPart < 0.85) {
-        EventParticle[j] = new Particle("K+");
-      } else if (chPart >= 0.85 && chPart < 0.9) {
-        EventParticle[j] = new Particle("K-");
-      } else if (chPart >= 0.9 && chPart < 0.945) {
-        EventParticle[j] = new Particle("P+");
-      } else if (chPart >= 0.945 && chPart < 0.99) {
-        EventParticle[j] = new Particle("P-");
-      } else if (chPart >= 0.99) {
-        EventParticle[j] = new Particle("K*");
-      }
-
+      EventParticle[j] = new Particle("a");
       // Part 2 set random impulse
       double phi       = TRandom::Uniform(0, TMath::2pi);
       double theta     = TRandom::Uniform(0, TMath::pi);
@@ -62,6 +44,27 @@ void random_generation()
       double impulse_y = impulse * TMath::sin(theta) * TMath::sin(phi);
       double impulse_z = impulse * TMath::cos(theta);
       EventParticle[j]->SetP(impulse_x, impulse_y, impulse_z);
+
+      // Part 3 Set type with probability
+      double chPart = TRandom::Uniform(0, 1);
+      // double chPart = TRandom::Rndm();
+      if (chPart < 0.4) {
+        EventParticle[j]->SetIndex("Pi+");
+      } else if (chPart >= 0.4 && chPart < 0.8) {
+        EventParticle[j]->SetIndex("Pi-");
+      } else if (chPart >= 0.8 && chPart < 0.85) {
+        EventParticle[j]->SetIndex("K+");
+      } else if (chPart >= 0.85 && chPart < 0.9) {
+        EventParticle[j]->SetIndex("K-");
+      } else if (chPart >= 0.9 && chPart < 0.945) {
+        EventParticle[j]->SetIndex("P+");
+      } else if (chPart >= 0.945 && chPart < 0.99) {
+        EventParticle[j]->SetIndex("P-");
+      } else if (chPart >= 0.99) {
+        EventParticle[j]->SetIndex("K*");
+      }
     }
+
+    // Part 5 fill the histograms
   }
 }
