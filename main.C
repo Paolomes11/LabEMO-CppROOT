b592dcc832ad4f2fc4833fdcdb8a67dcf9752e57
Giulio Mesini <200260paolo@gmail.com>
2024-10-24 12:06:25 +0200

    bolo

--- main.C
+++ main.C
@@ -1,10 +1,17 @@
-R__LOAD_LIBRARY(myRoot/Particle_cpp.so)
-R__LOAD_LIBRARY(myRoot/ParticleType_cpp.so)
-R__LOAD_LIBRARY(myRoot/ResonanceType_cpp.so)
+// clang-format off
+R__LOAD_LIBRARY(src/Particle_cpp.so)
+R__LOAD_LIBRARY(src/ParticleType_cpp.so)
+R__LOAD_LIBRARY(src/ResonanceType_cpp.so)
+// clang-format on
 
 void testso()
 {
-  Particle* A = new Particle("pollo");
+  Int_t nEv = 1E5;
+  Int_t nParticle = 120;
+  Particle EventParticles[nParticle];
+
+  Particle::AddParticleType("a", 1.34, +2, 2.);
+  Particle* A = new Particle("a");
   A->SetP(1., 1., 1.);
   A->GetPx();
 }
