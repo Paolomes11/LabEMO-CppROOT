fb01f05a467e69b79aac5834bcc62e289140b54a
Giulio Mesini <200260paolo@gmail.com>
2024-10-24 11:54:35 +0200

    random

--- src/random_generation.C
+++ src/random_generation.C
@@ -11,8 +11,9 @@ R__LOAD_LIBRARY(src/ResonanceType_cpp.so)
 
 void random_generation()
 {
-  int N    = 120;
-  int nGen = 1E5;
+  int Nbase = 100;
+  int Nmax  = 120;
+  int nGen  = 1E5;
 
   AddParticleType("pioni_positivi", 0.13957, 1.);
   AddParticleType("pioni_negativi", 0.13957, -1.);
@@ -24,15 +25,38 @@ void random_generation()
 
   gRandom→SetSeed();
 
-  Particle* EventParticle[N];
+  Particle* EventParticle[Nmax];
   for (int i = 1; i < nGen; i++) {
-    for (i = 1, i < 100, i++) {
-      double phi       = TRandom::Uniform(0, TMath::2pi);
-      double theta     = TRandom::Uniform(0, TMath::pi);
-      double impulse   = TRandom::Exp(1); // 1GeV
-      double impulse_x = impulse * TMath::sin(theta) * TMath::cos(phi);
-      double impulse_y = impulse * TMath::sin(theta) * TMath::sin(phi);
-      double impulse_z = impulse * TMath::cos(theta);
+    double impulse_x;
+    double impulse_y;
+    double impulse_z;
+
+    for (int j = 1, j < 100, j++) {
+      double phi     = TRandom::Uniform(0, TMath::2pi);
+      double theta   = TRandom::Uniform(0, TMath::pi);
+      double impulse = TRandom::Exp(1); // 1GeV
+      impulse_x      = impulse * TMath::sin(theta) * TMath::cos(phi);
+      impulse_y      = impulse * TMath::sin(theta) * TMath::sin(phi);
+      impulse_z      = impulse * TMath::cos(theta);
     }
+
+    // Part 3
+    double chPart = TRandom::Uniform(0, 1);
+    // double chPart = TRandom::Rndm();
+    if (chPart < 0.4) {
+      EventParticle[i] = new Particle("Pi+", impulse_x, impulse_y, impulse_z);
+    } else if (chPart >= 0.4 && chPart < 0.8) {
+      EventParticle[i] = new Particle("Pi-", impulse_x, impulse_y, impulse_z);
+    } else if (chPart >= 0.8 && chPart < 0.85) {
+      EventParticle[i] = new Particle("K+", impulse_x, impulse_y, impulse_z);
+    } else if (chPart >= 0.85 && chPart < 0.9) {
+      EventParticle[i] = new Particle("K-", impulse_x, impulse_y, impulse_z);
+    } else if (chPart >= 0.9 && chPart < 0.945) {
+      EventParticle[i] = new Particle("P+", impulse_x, impulse_y, impulse_z);
+    } else if (chPart >= 0.945 && chPart < 0.99) {
+      EventParticle[i] = new Particle("P-", impulse_x, impulse_y, impulse_z);
+    } else if (chPart >= 0.99) {
+      EventParticle[i] = new Particle("K*", impulse_x, impulse_y, impulse_z);
+    } 
   }
 }
