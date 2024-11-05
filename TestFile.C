7ef356909a73f3dfe32002a651580b7e03fbc206
Giulio Mesini <200260paolo@gmail.com>
2024-11-05 16:29:17 +0100

    now .gif

--- TestFile.C
+++ TestFile.C
@@ -7,21 +7,21 @@
 int TestFile()
 {
   // Apri il file ROOT
-  TFile* file = new TFile("histograms.root");
+  TFile* file = new TFile("src/histograms.root");
 
   TH1F* MyHist[12];
 
   // Leggi l'istogramma dal file
-  MyHist[0] = (TH1F*)file->Get("histo_particles");
-  MyHist[1] = (TH1F*)file->Get("histo_azimutal");
-  MyHist[2] = (TH1F*)file->Get("histo_polar");
-  MyHist[3] = (TH1F*)file->Get("histo_impulse");
-  MyHist[4] = (TH1F*)file->Get("histo_transverse_impulse");
-  MyHist[5] = (TH1F*)file->Get("histo_energy");
-  MyHist[6] = (TH1F*)file->Get("histo_invmass");
-  MyHist[7] = (TH1F*)file->Get("histo_invmass_disc");
-  MyHist[8] = (TH1F*)file->Get("histo_invmass_conc");
-  MyHist[9] = (TH1F*)file->Get("histo_invmass_Pi_K_disc");
+  MyHist[0]  = (TH1F*)file->Get("histo_particles");
+  MyHist[1]  = (TH1F*)file->Get("histo_azimutal");
+  MyHist[2]  = (TH1F*)file->Get("histo_polar");
+  MyHist[3]  = (TH1F*)file->Get("histo_impulse");
+  MyHist[4]  = (TH1F*)file->Get("histo_transverse_impulse");
+  MyHist[5]  = (TH1F*)file->Get("histo_energy");
+  MyHist[6]  = (TH1F*)file->Get("histo_invmass");
+  MyHist[7]  = (TH1F*)file->Get("histo_invmass_disc");
+  MyHist[8]  = (TH1F*)file->Get("histo_invmass_conc");
+  MyHist[9]  = (TH1F*)file->Get("histo_invmass_Pi_K_disc");
   MyHist[10] = (TH1F*)file->Get("histo_invmass_Pi_K_conc");
   MyHist[11] = (TH1F*)file->Get("histo_invmass_Ks_prod");
 
@@ -40,6 +40,8 @@ int TestFile()
     myCanvas->cd(i + 1);
     MyHist[i]->Draw();
   }
+
+  myCanvas->Print("Histograms.gif");
 
   // Mantieni la finestra aperta fino a quando non viene chiusa dall'utente
   gApplication->Run();
