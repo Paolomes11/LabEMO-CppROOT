be0420212326b08345b5e1488d0fcc4864479762
Giulio Mesini <200260paolo@gmail.com>
2024-11-07 15:02:48 +0100

    new hists

--- histo_diff.C
+++ histo_diff.C
@@ -1,7 +1,7 @@
 #include <TCanvas.h>
 #include <TF1.h>
 #include <TFile.h>
-#include <TH1D.h>
+#include <TH1F.h>
 #include <TLegend.h>
 #include <iostream>
 
@@ -13,37 +13,39 @@ void analisis()
     return;
   }
 
-  TH1D* histo_invmass_disc      = (TH1D*)file->Get("histo_invmass_disc");
-  TH1D* histo_invmass_conc      = (TH1D*)file->Get("histo_invmass_conc");
-  TH1D* histo_invmass_Pi_K_disc = (TH1D*)file->Get("histo_invmass_Pi_K_disc");
-  TH1D* histo_invmass_Pi_K_conc = (TH1D*)file->Get("histo_invmass_Pi_K_conc");
-  TH1D* histo_invmass_Ks_prod   = (TH1D*)file->Get("histo_invmass_Ks_prod");
+    TH1F* histograms[5];
 
-  if (histo_invmass_disc->GetNbinsX() != histo_invmass_conc->GetNbinsX()
-      || histo_invmass_Pi_K_disc->GetNbinsX() != histo_invmass_Pi_K_conc->GetNbinsX()) {
+  histograms[0]  = (TH1F*)file->Get("histo_invmass_disc");
+  histograms[1]  = (TH1F*)file->Get("histo_invmass_conc");
+  histograms[2]  = (TH1F*)file->Get("histo_invmass_Pi_K_disc");
+  histograms[3]  = (TH1F*)file->Get("histo_invmass_Pi_K_conc");
+  histograms[4]  = (TH1F*)file->Get("histo_invmass_Ks_prod");
+
+  if (histograms[0]->GetNbinsX() != histograms[1]->GetNbinsX()
+      || histograms[2]->GetNbinsX() != histograms[3]->GetNbinsX()) {
     std::cerr << "Histograms have different binning!" << std::endl;
     return;
   }
 
-  TH1D* result1_2 = (TH1D*)histo_invmass_disc->Clone("result1");
-  result1_2->Add(histo_invmass_conc, -1);
+  TH1F* result1_2 = (TH1F*)histograms[0]->Clone("result1");
+  result1_2->Add(histograms[1], -1);
 
-  TH1D* result3_4 = (TH1D*)histo_invmass_Pi_K_disc->Clone("result3");
-  result3_4->Add(histo_invmass_Pi_K_conc, -1);
+  TH1F* result3_4 = (TH1F*)histograms[2]->Clone("result3");
+  result3_4->Add(histograms[3], -1);
 
   TCanvas* canvas = new TCanvas("canvas", "Analisis of K*", 800, 600);
   result1_2->SetLineColor(kBlue);
   result3_4->SetLineColor(kRed);
-  histo_invmass_Ks_prod->SetLineColor(kGreen);
+  histograms[4]->SetLineColor(kGreen);
 
   result1_2->Draw();
   result3_4->Draw("SAME");
-  histo_invmass_Ks_prod->Draw("SAME");
+  histograms[4]->Draw("SAME");
 
   TLegend* legend = new TLegend(0.7, 0.7, 0.9, 0.9);
   legend->AddEntry(result1_2, "Subtract (1-2)", "l");
   legend->AddEntry(result3_4, "Subtract (3-4)", "l");
-  legend->AddEntry(histo_invmass_Ks_prod, "K* Decay", "l");
+  legend->AddEntry(histograms[4], "K* Decay", "l");
   legend->Draw();
 
   TF1* gaussianFit = new TF1("gaussianFit", "gaus", result3_4->GetXaxis()->GetXmin(), result3_4->GetXaxis()->GetXmax());
